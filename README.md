7deb3ab51a7b9dc43dd8af773575bdba7ec3e46f
Kai Karren <kai.karren@gmail.com>
2022-11-21 17:06:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Java Template
+
+Java Template repo using Gradle 7 and Java 11+.
+
---
+++ src/main/java/de/template/Main.java
@@ -0,0 +1,9 @@
+package de.template;
+
+public class Main {
+
+    public static void main(String... args){
+        System.out.println("Hello Template");
+    }
+
+}
